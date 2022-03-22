7bad465c7603db012377f5eaedf1842d09a1f9bf
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-22 00:49:43 -0600

    rending individual post

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from pdb import post_mortem
 from flask import Flask,render_template
 import requests
 
-post=requests.get("https://api.npoint.io/65036dfcf8a72fc447aa").json()
+posts=requests.get("https://api.npoint.io/65036dfcf8a72fc447aa").json()
 
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
-    print(post)
-    for i in post:
-        print(i)
-    return render_template('index.html',all_posts=post)
+    return render_template('index.html',all_posts=posts)
 
 @app.route("/about")
 def about():
@@ -19,6 +16,14 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def post(index):
+    identifier = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            identifier = blog_post
+    return render_template("post.html", post=identifier)
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
